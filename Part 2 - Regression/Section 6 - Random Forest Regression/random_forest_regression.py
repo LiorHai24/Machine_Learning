c907a533ac279baa71bf895459f1b59598b69faa
Lior <liorhay11@gmail.com>
2024-07-25 16:31:16 +0300

    finished random forest

--- Part 2 - Regression/Section 6 - Random Forest Regression/random_forest_regression.py
+++ Part 2 - Regression/Section 6 - Random Forest Regression/random_forest_regression.py
@@ -12,18 +12,16 @@ y = dataset.iloc[:, -1].values
 
 # Training the Random Forest Regression model on the whole dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
-
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)#number of trees
+regressor.fit(X,y)
 # Predicting a new result
-regressor.predict([[6.5]])
-
+print(regressor.predict([[6.5]]))
 # Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = np.arange(min(X), max(X), 0.1)#taking the indicated float values instead of only the the integers to get a smoother curve
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
+plt.scatter(X,y, color='red')#real data points
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')#the points(X_grid) and the prediction
+plt.title('Truth or Bluff (Random Forest regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
