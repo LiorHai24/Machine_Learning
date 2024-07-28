d66ead6ac3468e4e611bfb49e569e61f4b695fac
Lior <liorhay11@gmail.com>
2024-07-28 13:51:25 +0300

    start of classification, logistic regression

---
+++ Part 3 - Classification/Section 1 - Logistic Regression/logistic_regression.py
@@ -0,0 +1,30 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+
+
+# Feature Scaling
+
+# Training the Logistic Regression model on the Training set
+
+# Predicting a new result
+
+# Predicting the Test set results
+
+
+# Making the Confusion Matrix
+
+# Visualising the Training set results
+
+
+# Visualising the Test set results
