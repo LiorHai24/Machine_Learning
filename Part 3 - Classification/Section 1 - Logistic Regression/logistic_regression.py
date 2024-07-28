0a7d9d04505451deb316ca34335ec28cf3344a99
Lior <liorhay11@gmail.com>
2024-07-28 15:29:47 +0300

    finished Logistic regression

--- Part 3 - Classification/Section 1 - Logistic Regression/logistic_regression.py
+++ Part 3 - Classification/Section 1 - Logistic Regression/logistic_regression.py
@@ -11,20 +11,69 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-
+import sklearn.model_selection as model_selection
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size= 0.25, random_state= 0)#to use round number for the division of the dataset
 
 # Feature Scaling
-
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)# set the scale to match the train set and apply
+X_test = sc.transform(X_test)# apply the scale that was set on the train set for the test set
+print(X_train)
+print(X_test)
 # Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
 
 # Predicting a new result
+print(classifier.predict(sc.transform([[30,87000]])))#30 and 87000 needs to be scaled
 
 # Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1) , y_test.reshape(len(y_test),1)),axis=1))
+#prints the prediction and the actual values next to each other
 
-
-# Making the Confusion Matrix
-
+# Making the Confusion Matrix - shows the correct and incorrect predictions
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)#([65, 3] [8, 24]) 65 and 24 are the correct predictions, 3 and 8 are the incorrect predictions
+accuracy_score(y_test, y_pred)# the precentage of the correct predictions
 # Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train#inverse the scaling to get the original values
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                        np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))#create a grid with the min and max values of the X_set
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))#plot the prediction of the classifier on the grid
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):#plot the real values on the grid
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 # Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test#inverse the scaling to get the original values
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                        np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))#create a grid with the min and max values of the X_set
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))#plot the prediction of the classifier on the grid
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):#plot the real values on the grid
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+#a straight line that separates the two classes is for a linear classifier
+#for a better seperation we can use a non-linear classifier
