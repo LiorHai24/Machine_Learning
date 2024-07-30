a7d55a25a76262cf39f8bc45bb603427fcd5bb1c
Lior <liorhay11@gmail.com>
2024-07-30 14:24:01 +0300

    finished K-NN

--- Part 3 - Classification/Section 2 - K-Nearest Neighbors (K-NN)/k_nearest_neighbors.py
+++ Part 3 - Classification/Section 2 - K-Nearest Neighbors (K-NN)/k_nearest_neighbors.py
@@ -3,9 +3,13 @@
 #helps decide which category a new data point belongs to based on the majority of its K(usually 5)-nearest neighbors
 
 # Importing the libraries
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+print(os.getcwd())
+#os.chdir("Part 3 - Classification/Section 2 - K-Nearest Neighbors (K-NN)")
+#print(os.getcwd())
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -13,20 +17,70 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0) #random_state=0 to get the same result as the instructor
 
 # Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+print(X_train)
+print(X_test)
+
 
 # Training the K-NN model on the Training set
-
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2) #p=2 is for Euclidean distance
+classifier.fit(X_train, y_train)
 
 # Predicting a new result
+print(classifier.predict(sc.transform([[30,87000]])))#30 and 87000 needs to be scaled
 
 # Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1) , y_test.reshape(len(y_test),1)),axis=1))
 
 # Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
 
 
 # Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train#inverse the scaling to get the original values
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                        np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))#create a grid with the min and max values of the X_set
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))#plot the prediction of the classifier on the grid
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):#plot the real values on the grid
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 # Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test#inverse the scaling to get the original values
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                        np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))#create a grid with the min and max values of the X_set
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))#plot the prediction of the classifier on the grid
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):#plot the real values on the grid
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#problem running, the chart is very big, takes a long time to run, but works eventually :)
