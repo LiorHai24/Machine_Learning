a865c9efa2086b2535a92d2297bbe422b2b7c487
Lior <liorhay11@gmail.com>
2025-01-27 12:54:13 +0200

    starting naive_bayes

--- Part 3 - Classification/Section 5 - Naive Bayes/naive_bayes.py
+++ Part 3 - Classification/Section 5 - Naive Bayes/naive_bayes.py
@@ -29,18 +29,13 @@ print(X_test)
 
 # Training the Naive Bayes model on the Training set
 
+
 # Predicting a new result
-print(classifier.predict(sc.transform([[30,87000]])))
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
+
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
